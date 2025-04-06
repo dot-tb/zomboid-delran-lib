c38f42b6dfb787e51984fcd406b6ba6565ad8a35
Tibère Borel <tibere.borel@outlook.com>
2025-04-06 13:58:41 +0200

    Added mouseOverUi function and GetDebugPrint to get a dprint function

--- DelranUtils.lua
+++ DelranUtils.lua
@@ -7,4 +7,25 @@ function DelranUtils.IsBackpack(item)
     return item:IsInventoryContainer() and item:canBeEquipped() == "Back";
 end
 
+function DelranUtils.IsMouseOverUI()
+    local uis = UIManager.getUI()
+    for i = 0, uis:size() - 1 do
+        local ui = uis:get(i)
+        if ui:isMouseOver() then
+            return true;
+        end
+    end
+    return false;
+end
+
+---comment
+---@param moduleName string
+function DelranUtils.GetDebugPrint(moduleName)
+    return function(...)
+        if isDebugEnabled() then
+            print(moduleName, " ", ...);
+        end
+    end
+end
+
 return DelranUtils
